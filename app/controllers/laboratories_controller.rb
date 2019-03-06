fea140e5449bdeb61f7c2ca39729dffff712b4b0
daichi <b1504000@planet.kanazawa-it.ac.jp>
2019-03-06 05:29:48 +0000

    ログイン機能途中

--- app/controllers/laboratories_controller.rb
+++ app/controllers/laboratories_controller.rb
@@ -5,15 +5,9 @@ class LaboratoriesController < ApplicationController
   def show
   end
 
-  def new
-  end
-
   def edit
   end
 
   def update
-  end
-  
-  def create
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root 'pages#index'
-  resources :laboratories, except: [:destroy]
+  resources :laboratories, except: [:destroy, :new, :create]
+  # ログイン/ログアウト用
+  get  "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+  delete "sign_out", to: "sessions#destroy"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
