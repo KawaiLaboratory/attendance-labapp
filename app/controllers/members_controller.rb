037f837019310864962b8b4cf8d8a35376c08107
daichi <b1504000@planet.kanazawa-it.ac.jp>
2020-01-14 02:41:26 +0000

    labごとにmember参照

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -5,22 +5,22 @@ class MembersController < ApplicationController
     changed_at = DateTime.current
     # メソッド切ったり色々する
     if params[:go_cafeteria]
-      Member.where(status: 0..2).each do | member |
+      current_laboratory.members.where(status: 0..2).each do | member |
         if member.go_cafeteria?
           member.change_status(Member.statuses[:cafeteria], changed_at)
         end
       end
     elsif params[:finished_lanch]
-      Member.cafeteria.each do | member |
+      current_laboratory.members.cafeteria.each do | member |
         member.change_status(Member.statuses[:office], changed_at)
       end
     elsif params[:go_home]
-      Member.where(status: 0..6).each do | member |
+      current_laboratory.members.where(status: 0..6).each do | member |
         member.change_status(Member.statuses[:athome], changed_at)
       end
     else
       member_params.each do |id, member_param|
-        member = Member.find(id)
+        member = current_laboratory.members.find(id)
         if member.status != member_param[:status]
           member.change_status(member_param[:status], changed_at)
         end
